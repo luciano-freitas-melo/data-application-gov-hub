88ddd709feefb94bbd3463057b9fba4f1430b560
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2025-07-16 16:32:12 -0300

    fix/dbt-lista-servidores

--- airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/bronze/lista_servidores.sql
+++ airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/bronze/lista_servidores.sql
@@ -1,6 +1,6 @@
 with
     lista_servidores as (
-        select cpf, dt_ultima_transacao, cod_uorg
+        select cpf, dataultimatransacao as dt_ultima_transacao, coduorg as cod_uorg
         from {{ source("siape", "lista_servidores") }}
     )
 
