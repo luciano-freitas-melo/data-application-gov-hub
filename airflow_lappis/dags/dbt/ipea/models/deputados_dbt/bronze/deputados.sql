c24d0b00e074b7f453254b9109a19a2258b15d08
Mateushqms <mateushqms@gmail.com>
2026-03-04 14:49:04 -0300

    fix: ajustes na identacao e add url imagem

--- airflow_lappis/dags/dbt/ipea/models/deputados_dbt/bronze/deputados.sql
+++ airflow_lappis/dags/dbt/ipea/models/deputados_dbt/bronze/deputados.sql
@@ -9,6 +9,7 @@ with
             siglapartido::text as siglapartido,
             siglauf::text as siglauf,
             idlegislatura::integer as idlegislatura,
+            urlfoto::text as urlfoto,
             email::text as email,
             (dt_ingest || '-03:00')::timestamptz as dt_ingest
         from {{ source("dados_abertos", "deputados") }}
