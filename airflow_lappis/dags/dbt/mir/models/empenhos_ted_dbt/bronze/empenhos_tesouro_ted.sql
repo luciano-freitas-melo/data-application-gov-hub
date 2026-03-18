19729fabf9830755e26dc891c8411a7e9b9fb023
Mateushqms <mateushqms@gmail.com>
2026-03-18 13:39:41 -0300

    feat: camada bronze empenhos ted

---
+++ airflow_lappis/dags/dbt/mir/models/empenhos_ted_dbt/bronze/empenhos_tesouro_ted.sql
@@ -0,0 +1,37 @@
+{{ config(materialized="table") }}
+
+
+with
+    empenhos_teds_raw as (
+        select
+            programa_governo::text as programa_governo,
+            programa_governo_descricao::text as programa_governo_descricao,
+            acao_governo::text as acao_governo,
+            acao_governo_descricao::text as acao_governo_descricao,
+            emissao_mes::text as emissao_mes,
+            emissao_dia::text as emissao_dia,
+            ne_ccor::text as ne_ccor,
+            regexp_replace(ne_num_processo, '[./-]', '', 'g') as ne_num_processo,
+            ne_info_complementar::text as ne_info_complementar,
+            ne_ccor_descricao::text as ne_ccor_descricao,
+            doc_observacao::text as doc_observacao,
+            natureza_despesa::text as natureza_despesa,
+            natureza_despesa_descricao::text as natureza_despesa_descricao,
+            upper(ne_ccor_favorecido::text) as ne_ccor_favorecido,
+            ne_ccor_favorecido_descricao::text as ne_ccor_favorecido_descricao,
+            ne_ccor_ano_emissao::integer as ne_ccor_ano_emissao,
+            ptres::text as ptres,
+            fonte_recursos_detalhada::text as fonte_recursos_detalhada,
+            fonte_recursos_detalhada_descricao::text as fonte_recursos_detalhada_descricao,
+            {{ parse_financial_value("despesas_empenhadas") }} as despesas_empenhadas,
+            {{ parse_financial_value("despesas_liquidadas") }} as despesas_liquidadas,
+            {{ parse_financial_value("despesas_pagas") }} as despesas_pagas,
+            {{ parse_financial_value("restos_a_pagar_inscritos") }} as restos_a_pagar_inscritos,
+            {{ parse_financial_value("restos_a_pagar_pagos") }} as restos_a_pagar_pagos,
+            (dt_ingest || '-03:00')::timestamptz as dt_ingest
+        from {{ source("siafi", "empenhos_tesouro_parlamentares") }} 
+        where ne_ccor_ano_emissao ~ '^[0-9]{4}$'
+    )
+
+select *
+from empenhos_teds_raw
