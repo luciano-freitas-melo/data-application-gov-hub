ce91e81660c35b5e6e65a7bde441aebd911b8a9b
Luana <luana.comfort@gmail.com>
2026-06-08 14:59:54 -0300

    feat: adiciona data no cruzamento de convenio e bloqueio

--- airflow_lappis/dags/dbt/mir/models/siconv_dbt/silver/convenio_desbloqueio.sql
+++ airflow_lappis/dags/dbt/mir/models/siconv_dbt/silver/convenio_desbloqueio.sql
@@ -11,7 +11,9 @@ with
             sum(vl_desbloqueado) as total_desbloqueado,
             sum(vl_bloqueado) as total_bloqueado,
             sum(vl_total_desbloqueio) as total_valor_desbloqueio,
-            count(nr_ob) as qtd_ordens_bancarias
+            count(nr_ob) as qtd_ordens_bancarias,
+            min(data_cadastro) as primeiro_desbloqueio,
+            max(data_cadastro) as ultimo_desbloqueio
         from {{ ref("desbloqueio") }}
         group by nr_convenio
     )
@@ -21,6 +23,8 @@ select
     d.total_desbloqueado,
     d.total_bloqueado,
     d.total_valor_desbloqueio,
-    d.qtd_ordens_bancarias
+    d.qtd_ordens_bancarias,
+    d.primeiro_desbloqueio,
+    d.ultimo_desbloqueio
 from convenio c
 left join desbloqueio_agg d on c.nr_convenio = d.nr_convenio
